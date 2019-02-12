cefb209366cf7d4854d2853b5be7ab803f2be00c
Ellis Christiansen <elchrist@e4r10p7.21-school.ru>
2019-02-12 22:00:52 +0300

    evrthg w/ lists, added readme

--- remake_with_lists/main.c
+++ remake_with_lists/main.c
@@ -6,21 +6,30 @@
 /*   By: elchrist <elchrist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 21:45:07 by elchrist          #+#    #+#             */
-/*   Updated: 2019/02/11 22:28:54 by elchrist         ###   ########.fr       */
+/*   Updated: 2019/02/12 21:49:14 by elchrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fillit.h"
+
+void it_is_error(void)
+{
+    write(1, "error\n", 6);
+    exit(1);
+}
+
 int			main(int argc, char **argv)
 {
-	t_tetr	*tetris;
+	t_list	*tetris;
 	int		fd;
 
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
 		tetris = read_tetris(fd);
-		fillit(tetris); //какая-нибудь итоговая функция, в которую всё собрано
-		free_list(&tetris);
+		print_list(tetris); //функция просто для теста, выводит адреса)))0)
+		//fillit(tetris); //какая-нибудь итоговая функция, в которую всё собрано
+		//free_list(tetris);
 	}
 	return (0);
 }
--- remake_with_lists/reading.c
+++ remake_with_lists/reading.c
@@ -6,71 +6,93 @@
 /*   By: elchrist <elchrist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 20:56:24 by elchrist          #+#    #+#             */
-/*   Updated: 2019/02/11 22:35:31 by elchrist         ###   ########.fr       */
+/*   Updated: 2019/02/12 21:15:35 by elchrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void		free_tetr(t_tetr *head)
+#include "fillit.h"
+# include "libft/includes/libft.h"
+void	to_letters(char **tetr, char letter)
 {
-	t_tetr	*temp;
+	int i;
+	int j;
 
-	while (head != NULL)
-	{
-		temp = head;
-		head = head->next;
-		free(temp);
-	}
-}
-
-void		ft_listadd_to_end(t_tetr **begin_list, t_tetr *new)
-{
-	t_tetr	*current;
-
-	current = *begin_list;
-	if (current == NULL)
-		*begin_list = new;
-	else
-	{
-		while (current->next)
-			current = current->next;
-		current->next = new;
-	}
-}
-
-t_tetr		*create_tetr(char *str_tetr, char letter)
-{
-	int		i;
-	int		point;
-	t_tetr	tetr;
-
-	point = 0;
 	i = 0;
-	if (str_tetr == NULL)
-		it_is_error();
-	while (str_tetr[i] != '\0')
+	while (tetr[i] != NULL)
 	{
-		if (str_tetr[i] == '#')
+		j = 0;
+		while (tetr[i][j] != '\0')
 		{
-            //придумать как считать height и weight
-			// tetr.pos[point][0] = i % 5;
-			// tetr.pos[point][1] = i / 5;
-			tetr.letter = letter;
-			point++;
+			if (tetr[i][j] == '#')
+				tetr[i][j] = letter;
+			j++;
 		}
 		i++;
 	}
-    //надо добавить valid_check(&tetr);
-	//какой-нибудь normalize(&tetr);
+}
+
+char		**create_shape(char *buff, char letter)
+{
+	char	**temp;
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	k = 0;
+	if (!(temp = (char**)malloc(sizeof(char*) * 5)))
+		return (NULL);
+	temp[4] = NULL;
+	while (i < 4)
+	{
+		if (!(temp[i] = (char*)malloc(5)))
+			return (NULL);
+		j = 0;
+		while (j < 4)
+			temp[i][j++] = buff[k++];
+		temp[i++][j] = '\0';
+		k++;
+	}
+	valid_check(temp);
+	to_letters(temp, letter);
+	return (temp);
+}
+
+t_list		*create_tetr(char *buff, char letter)
+{
+	int		i;
+	int		height;
+	int		width;
+	t_tetr	tetr;
+
+	height = 1;
+	width = 1;
+	i = 0;
+	while (buff[i] != '\0')
+	{
+		if (buff[i] == '#')
+		{
+			if ((i < 19) && (buff[i + 1] == '#'))
+				width++;
+			if ((i < 14) && (buff[i + 5] == '#'))
+				height++;
+		}
+		i++;
+	}
+	tetr.height = height;
+	tetr.width = width;
+	tetr.letter = letter;
+	tetr.shape = create_shape(buff, letter);
 	return (ft_lstnew(&tetr, sizeof(t_tetr)));
 }
 
-t_tetr		*read_tetris(int fd)
+t_list		*read_tetris(int fd)
 {
 	int		ret;
 	char	letter;
 	char	buff[22];
-	t_tetr	*tetris;
-	t_tetr	*tetr;
+	t_list	*tetris;
+	t_list	*tetr;
 
 	if (fd < 0)
 		it_is_error();
@@ -79,11 +101,11 @@ t_tetr		*read_tetris(int fd)
 	while ((ret = read(fd, buff, 21)) && (ret != -1))
 	{
 		buff[ret] = '\0';
-        if ((ret != 21) && (buff[ret - 2] == '\n')) //или не ret-2, а 20 просто
+        if ((ret != 21) && (buff[ret - 2] == '\n')) //или не ret-2, а 20 просто?
     		it_is_error();
 		tetr = create_tetr(buff, letter++);
 		if (tetr == NULL)
-			free_tetr(&tetris);
+			free_list(tetris);
 		ft_listadd_to_end(&tetris, tetr);
 	}
 	return (tetris);
