76c34d7b2adc23b7f7c81430703b1bda6a5239ad
Christop Reek <creek@e2r12p4.21-school.ru>
2019-02-22 20:22:12 +0300

    norme

--- submission/solving.c
+++ submission/solving.c
@@ -6,27 +6,27 @@
 /*   By: creek <creek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/08 00:41:43 by creek             #+#    #+#             */
-/*   Updated: 2019/02/22 18:31:07 by creek            ###   ########.fr       */
+/*   Updated: 2019/02/22 19:38:10 by creek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "solving.h"
 
-int ft_sqrt_round_up(int nb)
+int		ft_sqrt_round_up(int nb)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while ((i * i) <= nb)
-    {
+	i = 0;
+	while ((i * i) <= nb)
+	{
 		if ((i * i) == nb)
-            return (i);
+			return (i);
 		i++;
-    }
-    return (i++);
+	}
+	return (i++);
 }
 
-void empty_map_drawing(char **map, int map_size)
+void	empty_map_drawing(char **map, int map_size)
 {
 	int i;
 
@@ -39,7 +39,7 @@ void empty_map_drawing(char **map, int map_size)
 	}
 }
 
-int solving(int quantity, t_list *tetris, char **map, int map_size)
+int		solving(int quantity, t_list *tetris, char **map, int map_size)
 {
 	int i;
 	int j;
@@ -55,14 +55,11 @@ int solving(int quantity, t_list *tetris, char **map, int map_size)
 			if (tetri_placing((t_tetr *)tetris->content, j, i, map, map_size))
 			{
 				tetri_drawing(i, j, map, (t_tetr *)tetris->content, map_size);
-				// for (int n = 0; n < map_size; n++)
-				// 	printf("%s\n", map[n]);
-				// printf("Мапа после отрисовки выше\n");
 				quantity--;
 				if (solving(quantity, tetris->next, map, map_size))
 					return (1);
 				remove_tetri((t_tetr *)tetris->content, map, j, i, map_size);
-        quantity++;
+				quantity++;
 			}
 			j++;
 		}
@@ -71,12 +68,13 @@ int solving(int quantity, t_list *tetris, char **map, int map_size)
 	return (0);
 }
 
-char** fillit(int quantity, t_list *tetris)
+char	**fillit(int quantity, t_list *tetris)
 {
-	int map_size;
-	char **map;
+	int		map_size;
+	char	**map;
+
 	map_size = ft_sqrt_round_up(quantity * 4);
-	if (!(map = (char **)malloc((sizeof(*map) * (map_size + 1))))) //size + 1 ??
+	if (!(map = (char **)malloc((sizeof(*map) * (map_size + 1)))))
 		return (0);
 	empty_map_drawing(map, map_size);
 	while (!(solving(quantity, tetris, map, map_size)))
@@ -90,195 +88,11 @@ char** fillit(int quantity, t_list *tetris)
 	return (map);
 }
 
-void cut_empty_lines(t_tetr *tetri)
+int		stupid_func(t_tetr *tetri)
 {
-	int str_counter = 0;
-	int i = 0;
-	int j = 0;
-	int new_i;
-	while (i < 3)
-	{
-		j = 0;
-		str_counter = 0;
-		while (j < 4)
-		{
-			if ((*tetri).shape[0][j] == '.')
-				str_counter++;
-			j++;
-		}
-		if (str_counter == 4)
-		{
-        	new_i = 0;
-        	while (new_i < 3)
-       		{
-          		ft_strncpy((*tetri).shape[new_i], (*tetri).shape[new_i + 1], 5);
-          		new_i++;
-       		}
-       		ft_strncpy((*tetri).shape[3], "....", 5); // not sure about this one, to be tested
-		}
-      i++;
-    }
-}
-
-void cut_empty_column(t_tetr *tetri)
-{
-	int col_counter = 0;
-	int i = 0;
-	int j = 0;
-	int new_i;
-	int new_j = 0;
-	while (j < 3)
-	{
-		i = 0;
-		col_counter = 0;
-		while (i < 4)
-		{
-			if ((*tetri).shape[i][0] == '.')
-				col_counter++;
-			i++;
-		}
-		if (col_counter == 4)
-		{
-			new_i = 0;
-			while (new_i < 4)
-			{
-				new_j = 0;
-				while (new_j < 3)
-				{
-					(*tetri).shape[new_i][new_j] = ((*tetri).shape[new_i][new_j + 1]);
-					new_j++;
-				}
-				(*tetri).shape[new_i][3] = '.';
-				new_i++;
-			}
-		}
-      j++;
-    }
-}
-
-int tetri_placing(t_tetr *tetri,  int x, int y,char **map, int map_size)
-{
-	int i;
-	int j;
-	int k;
-	cut_empty_lines(tetri);
-  	cut_empty_column(tetri);
-	// printf("ниже обрезанная тетри\n");
-	// for (int n = 0; n < 4; n++)
-	// 	printf("%s\n", (*tetri).shape[n]);
-	// printf("выше обрезанная тетри\n");
-  // // while (1 && tetri->letter == 'B') // этот бесконечный цикл для дебага
-  //   ;
-	// printf("I m here x %5d, y %5d, mapsize %5d\n", x, y, map_size);
-	if (map[y][x] != '.')
-		return (0);
-	i = -1;
-	k = 0;
-	while (++i < 4)
-	{
-		j = -1;
-		while (++j < 4)
-		{
-			if ((tetri->shape)[0][0] == '.' && k == 0 && x != 0)
-			{
-				k++;
-			// 	if ((tetri->shape)[0][1] == '.' && x > 1)
-			// 		k++;
-				if (stupid_func(tetri))
-					k++;
-			}
-
-			//printf("tetri i %4d, j %4d, value %c, letter %c \n", i,j,tetri->shape[i][j], tetri->letter);
-			if ((tetri->shape)[i][j] == tetri->letter)
-			{
-				//printf("args: x %4d, y %4d, i %4d, j %4d\n", x,y,i,j);
-				if (!((y + i >= 0) && (y + i < map_size) && (x + j - k >= 0) && (x + j - k < map_size)))
-					return (0);
-				if (map[y + i][x + j - k] != '.')
-					return(0);
-			}
-		}
-	}
-  // printf("тетри встала, верну 1\n");
-  // printf("буква tetri - %c\n", (*tetri).letter);
-	// printf("успешные координаты: %d - x, %d - y\n", x, y);
-	// //printf("%c - это символ мапы с координатами\n", map[y][x]);
-	// printf("мапа до отрисовки ниже\n");
-	// for (int n = 0; n < map_size; n++)
-	// 	printf("%s\n", map[n]);
-	// printf("\n");
-
-	return (1);
-}
-
-int stupid_func(t_tetr *tetri)
-{
-	if ((tetri->shape)[0][2] == tetri->letter && (tetri->shape[1][0] == tetri->letter) && (tetri->shape[1][1] == tetri->letter) && (tetri->shape[1][2] == tetri->letter))
+	if ((tetri->shape)[0][2] == tetri->letter && (tetri->shape[1][0] == tetri->\
+		letter) && (tetri->shape[1][1] == tetri->letter) && (tetri->shape[1][2]\
+			== tetri->letter))
 		return (1);
 	return (0);
-}
-
-int remove_tetri(t_tetr *tetri, char **map, int y, int x, int map_size)
-{
-	int i;
-	int j;
-
-	j = 0;
-	// y -= (*tetri).height;
-	// x -= (*tetri).width;
-	// while (j < (*tetri).height)
-	// {
-	// 	i = 0;
-	// 	while (i < (*tetri).width)
-	// 	{
-	// 		map[j][i] = '.';
-	// 		i++;
-	// 	}
-	// 	j++;
-	// }  // вот это стирание косячное
-  while (j < map_size)
-  {
-    i = 0;
-    while (i < map_size)
-    {
-      if (map [j][i] == (*tetri).letter)
-        map[j][i] = '.';
-      i++;
-    }
-    j++;
-  }
-  // printf("мапа после зачистки ниже\n");
-  // for (int n = 0; n < map_size; n++)
-  //   printf("%s\n", map[n]);
-	return (0);
-}
-
-int tetri_drawing(size_t y, size_t x, char **map, t_tetr *tetri, int map_size)
-{
-	size_t i;
-	size_t j;
-  j = 0;
-  i = 0;
-	int k = 0;
-	while (j < (*tetri).height && (y + j) < map_size)
-	{
-		i = 0;
-		if ((tetri->shape)[0][0] == '.' && k == 0 && x != 0)
-		{
-			k++;
-		// 	if ((tetri->shape)[0][1] == '.' && k > 1)
-		// 		k++;
-			if (stupid_func(tetri))
-				k++;
-		}
-		//printf("%d - eto vyvod stupid_func\n", stupid_func(tetri));
-		while (i < (*tetri).width && (x + i - k) <= map_size) // если здесь добавить -k, то отрисовка нормальная
-		{
-			if (((*tetri).shape[j][i] == (*tetri).letter) && map[y + j][x + i - k] == '.')
-				map[y + j][x + i - k] = (*tetri).shape[j][i];
-			i++;
-		}
-		j++;
-	}
-	return (1);
 }
--- submission/solving.h
+++ submission/solving.h
@@ -6,7 +6,7 @@
 /*   By: creek <creek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 15:31:04 by creek             #+#    #+#             */
-/*   Updated: 2019/02/22 18:05:53 by creek            ###   ########.fr       */
+/*   Updated: 2019/02/22 19:25:34 by creek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,25 @@
 #include "libft.h"
 #include "fillit.h"
 
-// typedef struct  s_tetr
-// {
-//     int      height;
-//     int      width;
-//     char        **shape;
-//     char        letter;
-//   struct s_tetr *next;
-// }               t_tetr;
 
-// void	ft_listadd_to_end(t_tetr **begin_list, t_tetr *new);
 void		free_tetr(t_list *head);
 
 int ft_sqrt_round_up(int nb);
 
 void empty_map_drawing(char **map, int map_size);
 
-char **fillit(int quantity, t_list *tetris);
+int solving(int quantity, t_list *tetris, char **map, int map_size);
 
-//int fitting(char **map, t_tetr **tetri, int map_size, int quantity);
+char** fillit(int quantity, t_list *tetris);
 
-// int map_iterating(t_tetr *tetri, char **map, int map_size);
+void cut_empty_lines(t_tetr *tetri);
 
-int tetri_drawing(size_t y, size_t x, char **map, t_tetr *tetri, int map_size);
+void cut_empty_column(t_tetr *tetri);
 
-//int tetri_placing(t_tetr *tetri, int y, int x, char **map);
-int tetri_placing(t_tetr *tetri, int y, int x, char **map, int map_size);
+int tetri_placing(t_tetr *tetri,  int x, int y,char **map, int map_size);
+
+int stupid_func(t_tetr *tetri);
 
 int remove_tetri(t_tetr *tetri, char **map, int y, int x, int map_size);
 
-int stupid_func(t_tetr *tetri);
+int tetri_drawing(size_t y, size_t x, char **map, t_tetr *tetri, int map_size);
